474f0ad65afb9d7c787a6118b5b980a119ee00db
Jules <jules.duarte@edu.esiee-it.fr>
2024-07-06 21:47:47 +0200

    Updates

--- server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/julesduarte/Documents/Cours/Cours_L3/Akashic/src/app/layout":["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"1ZHoo27O+cU6AhIaMGJ9ng6YOE3F5q76PeSmo+sbrsM=\"}"
