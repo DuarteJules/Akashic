d5707052465f0f60ef610350e05beb7afb1408eb
Jules <jules.duarte@edu.esiee-it.fr>
2024-07-07 15:15:36 +0200

    correction eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 import Typography from '@/components/ui/typography'
 import Image from 'next/image'
 import Feature from './feature'
-import { Workflow, Users, BookHeart, ThumbsUp } from 'lucide-react'
+import { Users, BookHeart, ThumbsUp } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Home() {
